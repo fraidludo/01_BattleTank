86c791091dcdbc9f0e787b6743d49557c10c2c77
DESKTOP-EKG57A9\Fraid <chena.f@hotmail.fr>
2017-12-06 19:40:11 +0100

    BT04 Virtual and Override vid 117

--- BattleTank/Source/BattleTank/Public/TankPlayerController.h
+++ BattleTank/Source/BattleTank/Public/TankPlayerController.h
@@ -14,9 +14,14 @@ UCLASS()
 class BATTLETANK_API ATankPlayerController : public APlayerController
 {
 	GENERATED_BODY()
-	
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
 public: 
 	ATank* GetControlledTank() const;
-	
-	
+
+
+		
 };
