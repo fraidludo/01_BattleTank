235381ce76f18a1ecfc9c4fa5632d08a34a5c570
DESKTOP-EKG57A9\Fraid <chena.f@hotmail.fr>
2017-12-08 17:05:24 +0100

    BT07 tick to the player controller vid 120

--- BattleTank/Source/BattleTank/Public/TankPlayerController.h
+++ BattleTank/Source/BattleTank/Public/TankPlayerController.h
@@ -15,13 +15,15 @@ class BATTLETANK_API ATankPlayerController : public APlayerController
 {
 	GENERATED_BODY()
 
-protected:
-	// Called when the game starts or when spawned
-	virtual void BeginPlay() override;
-
 public: 
 	ATank* GetControlledTank() const;
 
+	virtual void BeginPlay() override;
 
+	virtual void Tick(float DeltaTime) override;
+
+	// Start the tank moving the barrel so that a shot would hit where
+	// the crosshair intersects the world
+	void AimTowardsCrosshair();
 		
 };
