0a3f7fbda70b8bb9d7a8b68b85f16d43aefd44a1
DESKTOP-EKG57A9\Fraid <chena.f@hotmail.fr>
2017-12-09 16:20:50 +0100

    BT08 localisation du pixel à l'ecran responsive vid 122

--- BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
@@ -33,8 +33,25 @@ ATank* ATankPlayerController::GetControlledTank() const
 void ATankPlayerController::AimTowardsCrosshair()
 {
 	if (!GetControlledTank()) { return; }
+	
+	FVector HitLocation; // Out parameter
+	if (GetSightRayHitLocation(HitLocation)) // Has "side-effect", is going to line trace
+	{ 
+	// UE_LOG(LogTemp, Warning, TEXT("Look direction: %s"), *HitLocation.ToString());
+	// TODO Tell controlled tank to aim at this point
+	}
+}
 
-	// Get world location if linetrace through crosshair
-	// If it hits the landscape
-		// Tell controlled tank to aim at this point
+// Get world location of linetrace through crosshair true if hits landscape
+bool ATankPlayerController::GetSightRayHitLocation(FVector & HitLocation) const
+{
+	// find the crosshair position in pixel coordinates
+	int32 ViewportSizeX, ViewportSizeY;
+	GetViewportSize(ViewportSizeX, ViewportSizeY);
+
+	auto ScreenLocation = FVector2D(ViewportSizeX * CrossHairXLocation, ViewportSizeY * CrossHairYLocation);
+
+	// "De-project" the screen position of the crosshair to a world direction
+	// Line trace along that look direction, and see what we hit (up to max range)
+	return true;
 }
