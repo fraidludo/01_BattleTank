b9ac33225cf90f8f2e808d326e09f403a646818b
DESKTOP-EKG57A9\Fraid <chena.f@hotmail.fr>
2018-01-10 19:04:07 +0100

    BT23 quit button vid 147

--- BattleTank/Source/BattleTank/Public/TankPlayerController.h
+++ BattleTank/Source/BattleTank/Public/TankPlayerController.h
@@ -34,14 +34,16 @@ private:
 
 	bool GetSightRayHitLocation(FVector& HitLocation) const;
 
-	UPROPERTY(EditAnywhere)
-		float LineTraceRange = 1000000;
+	
+	UPROPERTY(EditDefaultsOnly)
+	float CrossHairXLocation = 0.5;
 
-	UPROPERTY(EditAnywhere)
-		float CrossHairXLocation = 0.5;
+	UPROPERTY(EditDefaultsOnly)
+	float CrossHairYLocation = 0.33333;
 
-	UPROPERTY(EditAnywhere)
-		float CrossHairYLocation = 0.33333;
+	UPROPERTY(EditDefaultsOnly)
+	float LineTraceRange = 1000000;
+
 
 	bool GetLookDirection(FVector2D ScreenLocation, FVector& LookDirection) const;
 	bool GetLookVectorHitLocation(FVector LookDirection, FVector& HitLocation)const;
